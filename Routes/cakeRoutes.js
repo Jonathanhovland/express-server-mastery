9e126e00a681378cc96b70f2c6ef9d11f2fdc762
jonathanhovland <jonathan.hovland@yahoo.com>
2018-10-26 13:31:16 -0600

    Added Push Route

--- Routes/cakeRoutes.js
+++ Routes/cakeRoutes.js
@@ -9,15 +9,40 @@ router.get("/", (req, res) => {
 })
 
 router.get("/:id" , (req, res, next) => {
-
+    
     let chosenCake = cakes.filter(cake => cake.id == req.params.id)
     
     if (!chosenCake.length) {
-        console.log("🥨")
-        next()
+        return next()
     }
 
     res.json({ cake: chosenCake[0] })
 })
+
+router.post("/", (req, res) => {
+    const newCake = req.body
+
+    cakes.push(newCake)
+
+    res.json({ cakes: cakes })
+})
+
+router.put("/:id", (req, res) => {
+    const body = req.body
+
+    const updatedCakes = cakes.filter(cake => {
+
+        if (cake.id == req.params.id) {
+            return body
+        }
+        return cake
+    })
+
+    res.json({ cakes: updatedCakes })
+})
+
+router.delete
+
+
 
 module.exports = router
--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -8,11 +8,39 @@ router.get("/", (req, res) => {
     res.json({ students : students })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
     
   let chosenStudent = students.filter(student => student.id == req.params.id)
+
+  if(!chosenStudent.length){
+    return next()
+  }
+
   res.json({ student : chosenStudent })
 
 })
+
+router.post("/", (req, res) => {
+    const newStudent = req.body
+
+    students.push(newStudent)
+
+    res.json({ students: students })
+})
+
+router.put("/:id", (req, res) => {
+    const body = req.body
+
+    const updatedStudents = students.map(student => {
+
+        if (student.id == req.params.id) {
+            return body
+        }
+        return student
+    })
+
+    res.json({ students: updatedStudents })
+})
+
 
 module.exports = router
