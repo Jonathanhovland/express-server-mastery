05eeede6a326f9d22eaac3401a1cd22ed22fe5e3
jonathanhovland <jonathan.hovland@yahoo.com>
2018-10-26 11:58:55 -0600

    Added error handling

--- Routes/cakeRoutes.js
+++ Routes/cakeRoutes.js
@@ -8,4 +8,16 @@ router.get("/", (req, res) => {
     res.json({ cakes: cakes })
 })
 
+router.get("/:id" , (req, res, next) => {
+
+    let chosenCake = cakes.filter(cake => cake.id == req.params.id)
+    
+    if (!chosenCake.length) {
+        console.log("🥨")
+        next()
+    }
+
+    res.json({ cake: chosenCake[0] })
+})
+
 module.exports = router
--- app.js
+++ app.js
@@ -17,10 +17,22 @@ app.get("/", (req, res) => {
     res.json("🥨")
 })
 
-
 app.use("/cakes", cakeRoutes)
 app.use("/students", studentRoutes)
 
+app.use(notFound)
+
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
 
 
 
